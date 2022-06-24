25eca9dffd3435ae7bfe83e3a5abceca4bc83097
Leonardo Leite <84483790+roxleopardo@users.noreply.github.com>
2022-06-24 20:11:10 -0300

    Update run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -3,5 +3,4 @@
 docker stop queue-watcher
 docker rm queue-watcher
 docker build -t queue-watcher .
-docker run --cap-add SYS_NICE  --security-opt seccomp:unconfined --name="queue-watcher" -d queue-watcher
-docker exec -it queue-watcher bash
+docker run --cap-add SYS_NICE  --security-opt seccomp:unconfined --restart unless-stopped --name="queue-watcher" -d queue-watcher
