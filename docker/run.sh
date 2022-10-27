974f55518e264f0ea6ea8008fc150d99ca1b1144
Leonardo Leite <84483790+roxleopardo@users.noreply.github.com>
2022-10-27 10:03:30 -0300

    Update run.sh
    Changed restart container parameter

--- docker/run.sh
+++ docker/run.sh
@@ -3,4 +3,4 @@
 docker stop queue-watcher
 docker rm queue-watcher
 docker build -t queue-watcher .
-docker run --cap-add SYS_NICE  --security-opt seccomp:unconfined --restart unless-stopped --name="queue-watcher" -d queue-watcher
+docker run --cap-add SYS_NICE  --security-opt seccomp:unconfined --restart always --name="queue-watcher" -d queue-watcher
